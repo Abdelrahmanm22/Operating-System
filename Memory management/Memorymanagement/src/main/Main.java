1be62337656e96c9eba9e9f21324e4a4b439ef2a
Abdelrahmanm22 <abdra1396@gmail.com>
2023-01-14 01:23:50 +0200

    first commit

---
+++ Memory management/Memorymanagement/src/main/Main.java
@@ -0,0 +1,55 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		Scanner str = new Scanner(System.in);
+		IAlgorithm algo ;
+		ArrayList<Partition> partitions = new ArrayList<>();
+		
+		
+		System.out.print("Enter number of partition: ");
+		int no_of_partitions = in.nextInt();
+		for(int i=0;i<no_of_partitions;i++) {
+			System.out.print("Enter Partition size: ");
+			int  s = in.nextInt();
+			
+			partitions.add(new Partition("Partition"+i, s, null));
+		}
+		
+		ArrayList<Process> processList = new ArrayList<>();
+		System.out.print("Enter number of process: ");
+		int no_of_processes = in.nextInt();
+		
+		for(int i=0;i<no_of_processes;i++) {
+			System.out.print("Process name and its size: ");
+			String name = str.nextLine();
+			int s= in.nextInt();
+			processList.add(new Process(name, s));
+		}
+		
+
+		System.out.println("Select the policy you want to apply:");
+		System.out.println("1. First fit");
+		System.out.println("2. Worst fit");
+		System.out.println("3. Best fit");
+		System.out.print("option:");
+		int option= in.nextInt();
+		if(option==1) {
+			algo = new FirstFit();
+			algo.run(partitions, processList);
+		}else if(option==2) {
+			algo = new WorstFit();
+			algo.run(partitions, processList);
+		}else if(option==3) {
+			algo = new BestFit();
+			algo.run(partitions, processList);
+		}
+	}
+	
+	
+}
